b2306f2766fa0029612df33da8f19a76b5ab85b6
elisezhg <elise.zheng@hotmail.fr>
2022-07-07 22:13:08 -0400

    Refactoring and add game end logic

--- js/index.js
+++ js/index.js
@@ -1,61 +1,99 @@
-let BOARD_SIZE = 5;
-
-const randomTileValues = [2, 4];
+let BOARD_SIZE;
 let state;
 
-// TODO
-function checkGameStatus() {
-  if (state.includes(2048)) {
-    alert('You won 🤩');
-    return 1;
-  }
-  if (state.includes(0)) {
-    return 0;
-  }
-  // BUG: loss only if board is full and no more moves are possible
-  alert('You lost 😔');
-  return -1;
+function isAnimationInProgress() {
+  return $('.tile')
+    .toArray()
+    .some((elem) => $(elem).attr('class').indexOf('move') !== -1);
 }
 
-function addToScore(value) {
-  const currentScore = +$('#score').text();
-  $('#score').text(currentScore + value);
+function incrementMoves() {
+  $('#total-moves').text(+$('#total-moves').text() + 1);
 }
 
-function updateState(noTile, value) {
-  state[noTile] = value;
-  $(`.board-layout-tiles .tile:eq(${noTile})`)
-    .html(value)
-    .addClass(`tile-${value} appear`);
-
-  setTimeout(
-    () => $(`.board-layout-tiles .tile:eq(${noTile})`).removeClass('appear'),
-    200
+function transposeBoard() {
+  state = state.map(
+    (_, idx) =>
+      state[(idx % BOARD_SIZE) * BOARD_SIZE + Math.floor(idx / BOARD_SIZE)]
   );
+}
+
+function isMergePossible() {
+  for (let col = 0; col < BOARD_SIZE; col++) {
+    let curr = col;
+    for (let row = 0; row < BOARD_SIZE; row++) {
+      const idx = col + row * BOARD_SIZE;
+
+      if (curr >= idx) continue;
+
+      // A merge is possible!
+      if (state[curr] === state[idx]) {
+        return true;
+      }
+
+      curr += BOARD_SIZE;
+    }
+  }
+
+  return false;
+}
+
+function checkGameStatus() {
+  // Won
+  if (state.includes(2048)) {
+    alert('You won 🥳!');
+    initGame();
+  }
+
+  // Check for empty tiles
+  if (state.includes(0)) {
+    return;
+  }
+
+  // Check for merges vertically and horizontally
+  const mergePossibleV = isMergePossible();
+  transposeBoard();
+  const mergePossibleH = isMergePossible();
+  transposeBoard();
+
+  if (!mergePossibleV && !mergePossibleH) {
+    alert('Game over 😔...');
+    initGame();
+  }
 }
 
 function getRandomTile() {
   let tile;
   do {
     tile = Math.floor(Math.random() * state.length);
-  } while (
-    checkGameStatus() === 0 &&
-    (tile === undefined || state[tile] !== 0)
-  );
+  } while (state[tile] !== 0);
   return tile;
 }
 
-function getRandomValue() {
-  return randomTileValues[Math.floor(Math.random() * randomTileValues.length)];
-}
-
-function generateRandomTile() {
+function generateNewTile() {
   setTimeout(() => {
-    updateState(getRandomTile(), getRandomValue());
+    const tile = getRandomTile();
+    const value = Math.random() < 0.8 ? 2 : 4;
+
+    state[tile] = value;
+
+    $(`.board-layout-tiles .tile:eq(${tile})`)
+      .html(value)
+      .addClass(`tile-${value} appear`);
+
+    setTimeout(() => {
+      $(`.board-layout-tiles .tile:eq(${tile})`).removeClass('appear');
+      checkGameStatus();
+    }, 200);
   }, 100);
 }
 
 function initGame() {
+  // Set board size
+  const size = +$('#board-size').val();
+  BOARD_SIZE = size;
+  $(':root').css('--board-size', size);
+
   // Fill board
   $('.board-layout').empty();
   for (let i = 0; i < BOARD_SIZE ** 2; i++) {
@@ -64,59 +102,68 @@ function initGame() {
 
   // Initialize state
   state = new Array(BOARD_SIZE ** 2).fill(0);
-  $('#score').text(0);
+  $('#total-moves').text(0);
 
-  generateRandomTile();
-  generateRandomTile();
+  generateNewTile();
+  generateNewTile();
 }
 
-function rotateBoard() {
-  state = state.map(
-    (_, idx) =>
-      state[(idx % BOARD_SIZE) * BOARD_SIZE + Math.floor(idx / BOARD_SIZE)]
-  );
-}
-
-function animate(idx, curr, direction, pop) {
-  let idx2;
-  let curr2;
-  let movingUnit;
+function getAbsolutePositions(fromIdx, toIdx, direction) {
+  let start;
+  let end;
 
   switch (direction) {
     case 'up':
-      idx2 = idx;
-      curr2 = curr;
-      movingUnit = (curr - idx) / BOARD_SIZE;
-      break;
+      return {
+        start: fromIdx,
+        end: toIdx,
+        movingUnit: (toIdx - fromIdx) / BOARD_SIZE,
+      };
     case 'down':
-      idx2 = BOARD_SIZE ** 2 - idx - 1;
-      curr2 = BOARD_SIZE ** 2 - curr - 1;
-      movingUnit = (curr2 - idx2) / BOARD_SIZE;
-      break;
+      return {
+        start: BOARD_SIZE ** 2 - fromIdx - 1,
+        end: BOARD_SIZE ** 2 - toIdx - 1,
+        movingUnit: (fromIdx - toIdx) / BOARD_SIZE,
+      };
     case 'left':
-      idx2 = (idx % BOARD_SIZE) * BOARD_SIZE + Math.floor(idx / BOARD_SIZE);
-      curr2 = (curr % BOARD_SIZE) * BOARD_SIZE + Math.floor(curr / BOARD_SIZE);
-      movingUnit = curr2 - idx2;
-      break;
+      start =
+        (fromIdx % BOARD_SIZE) * BOARD_SIZE + Math.floor(fromIdx / BOARD_SIZE);
+      end = (toIdx % BOARD_SIZE) * BOARD_SIZE + Math.floor(toIdx / BOARD_SIZE);
+      return {
+        start,
+        end,
+        movingUnit: end - start,
+      };
     case 'right':
-      idx2 = BOARD_SIZE ** 2 - idx - 1;
-      curr2 = BOARD_SIZE ** 2 - curr - 1;
-      idx2 = (idx2 % BOARD_SIZE) * BOARD_SIZE + Math.floor(idx2 / BOARD_SIZE);
-      curr2 =
-        (curr2 % BOARD_SIZE) * BOARD_SIZE + Math.floor(curr2 / BOARD_SIZE);
-      movingUnit = curr2 - idx2;
-      break;
+      start = BOARD_SIZE ** 2 - fromIdx - 1;
+      end = BOARD_SIZE ** 2 - toIdx - 1;
+      start =
+        (start % BOARD_SIZE) * BOARD_SIZE + Math.floor(start / BOARD_SIZE);
+      end = (end % BOARD_SIZE) * BOARD_SIZE + Math.floor(end / BOARD_SIZE);
+      return {
+        start,
+        end,
+        movingUnit: end - start,
+      };
     default:
-      break;
+      return {};
   }
+}
 
-  // Amimate non empty tile
+function animateTile(fromIdx, toIdx, direction, popTile) {
+  const { start, end, movingUnit } = getAbsolutePositions(
+    fromIdx,
+    toIdx,
+    direction,
+  );
+
+  // Amimate tile if non-empty
   if (
-    $(`.board-layout-tiles .tile:eq(${idx2})`)
+    $(`.board-layout-tiles .tile:eq(${start})`)
       .attr('class')
       .indexOf('tile-') !== 1
   ) {
-    $(`.board-layout-tiles .tile:eq(${idx2})`)
+    $(`.board-layout-tiles .tile:eq(${start})`)
       .addClass(
         `move-${
           direction === 'up' || direction === 'down' ? 'vertical' : 'horizontal'
@@ -125,34 +172,32 @@ function animate(idx, curr, direction, pop) {
       .css('--moving-unit', movingUnit);
   }
 
-  const tmpCurr = curr2;
-  const tmpIdx = idx2;
-
+  // Set timer to erase old tile and display new tile after the animation is done
   setTimeout(() => {
     // Erase old tile
-    $(`.board-layout-tiles .tile:eq(${tmpIdx})`)
+    $(`.board-layout-tiles .tile:eq(${start})`)
       .html('')
       .css('--moving-unit', '')
       .removeClass()
       .addClass('tile');
 
     // Place new tile
-    if (state[tmpCurr]) {
-      $(`.board-layout-tiles .tile:eq(${tmpCurr})`)
-        .html(state[tmpCurr])
+    if (state[end]) {
+      $(`.board-layout-tiles .tile:eq(${end})`)
+        .html(state[end])
         .removeClass()
-        .addClass(`tile tile-${state[tmpCurr]} ${pop ? 'pop' : ''}`);
+        .addClass(`tile tile-${state[end]} ${popTile ? 'pop' : ''}`);
     } else {
-      $(`.board-layout-tiles .tile:eq(${tmpCurr})`)
+      $(`.board-layout-tiles .tile:eq(${end})`)
         .html('')
         .removeClass()
         .addClass(`tile`);
     }
 
     // Set timer to remove pop class
-    if (pop) {
+    if (popTile) {
       setTimeout(
-        () => $(`.board-layout-tiles .tile:eq(${tmpCurr})`).removeClass('pop'),
+        () => $(`.board-layout-tiles .tile:eq(${end})`).removeClass('pop'),
         200
       );
     }
@@ -160,128 +205,154 @@ function animate(idx, curr, direction, pop) {
 }
 
 function compress(direction) {
-  let generateNewTile = false;
+  let boardChanged = false;
 
   for (let col = 0; col < BOARD_SIZE; col++) {
     let curr = col;
 
     for (let row = 0; row < BOARD_SIZE; row++) {
-      const idx = col + row * BOARD_SIZE;
+      const target = col + row * BOARD_SIZE;
 
-      if (curr >= idx || state[idx] === 0) continue;
+      // Skip empty target tiles
+      if (state[target] === 0) continue;
 
-      if (state[curr] === 0) {
-        state[curr] = state[idx];
-        state[idx] = 0;
-        animate(idx, curr, direction);
-        generateNewTile = true;
-      } else if (state[curr] === state[idx]) {
-        state[curr] *= 2; // Merge tiles
-        state[idx] = 0;
-        animate(idx, curr, direction, true);
-        addToScore(state[curr]);
+      // Find empty tile or mergeable tile
+      while (
+        state[curr] !== 0 &&
+        state[curr] !== state[target] &&
+        curr < target
+      )
         curr += BOARD_SIZE;
-        generateNewTile = true;
+      if (curr >= target) continue;
+
+      // Merge
+      if (state[curr] === state[target]) {
+        state[curr] *= 2;
+        state[target] = 0;
+        animateTile(target, curr, direction, true);
+        curr += BOARD_SIZE;
+
+        // Move target tile to empty tile
       } else {
-        curr += BOARD_SIZE;
-        if (curr !== idx) {
-          state[curr] = state[idx];
-          state[idx] = 0;
-          animate(idx, curr, direction);
-          generateNewTile = true;
-        }
+        state[curr] = state[target];
+        state[target] = 0;
+        animateTile(target, curr, direction);
       }
+
+      boardChanged = true;
     }
   }
-  return generateNewTile;
+
+  if (boardChanged) {
+    incrementMoves();
+    generateNewTile();
+  }
 }
 
 function compressUp() {
-  const generateNewTile = compress('up');
-  if (generateNewTile) generateRandomTile();
-  // checkGameStatus();
+  compress('up');
 }
 
 function compressDown() {
   state = state.reverse();
-  const generateNewTile = compress('down');
+  compress('down');
   state = state.reverse();
-  if (generateNewTile) generateRandomTile();
-  // checkGameStatus();
 }
 
 function compressLeft() {
-  rotateBoard();
-  const generateNewTile = compress('left');
-  rotateBoard();
-  if (generateNewTile) generateRandomTile();
-  // checkGameStatus();
+  transposeBoard();
+  compress('left');
+  transposeBoard();
 }
 
 function compressRight() {
   state = state.reverse();
-  rotateBoard();
-  const generateNewTile = compress('right');
-  rotateBoard();
+  transposeBoard();
+  compress('right');
+  transposeBoard();
   state = state.reverse();
-  if (generateNewTile) generateRandomTile();
-  // checkGameStatus();
+}
+
+// Debug function
+function update() {
+  state.forEach((value, idx) => {
+    if (value === 0) {
+      $(`.board-layout-tiles .tile:eq(${idx})`)
+        .html('')
+        .removeClass()
+        .addClass('tile');
+    } else {
+      $(`.board-layout-tiles .tile:eq(${idx})`)
+        .html(value)
+        .removeClass()
+        .addClass(`tile tile-${value}`);
+    }
+  });
+}
+
+// Debug function
+function setBoardGameOver() {
+  state = [8, 32, 8, 32, 16, 4, 16, 4, 8, 32, 8, 32, 16, 4, 16, 0];
+  update();
+}
+
+// Debug function
+function setBoardWin() {
+  state = [1024, 0, 1024, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  update();
 }
 
 $(() => {
-  BOARD_SIZE = +$('#board-size').val();
-
-  // Listen to board size change
-  $('#board-size').change(() => {
-    BOARD_SIZE = +$('#board-size').val();
-    $(':root').css('--board-size', BOARD_SIZE);
-    initGame();
-  });
-
   // Initialize the game
   initGame();
 
+  // Listen to board size change
+  $('#board-size').change(initGame);
+
   // Reset the game
-  $('#reset').click(() => initGame());
+  $('#reset').click(initGame);
 
   // Handle keypresses
   $(document).keyup((e) => {
-    // Do not process if animation is in progress
-    let animationInProgress = false;
+    // Do not process if an animation is in progress
+    if (isAnimationInProgress()) return;
 
-    $('.tile').each((_, elem) => {
-      if ($(elem).attr('class').indexOf('move') !== -1) {
-        animationInProgress = true;
-      }
-    });
-
-    if (animationInProgress) return;
-
-    switch (e.which) {
-      case 87:
-      case 38:
+    switch (e.key.toLowerCase()) {
+      case 'w':
+      case 'up':
         compressUp();
         break;
-      case 83:
-      case 40:
+      case 's':
+      case 'down':
         compressDown();
         break;
-      case 65:
-      case 37:
+      case 'a':
+      case 'left':
         compressLeft();
         break;
-      case 68:
-      case 39:
+      case 'd':
+      case 'right':
         compressRight();
         break;
-      case 82:
+      case 'r':
         initGame();
+        break;
+      case '1':
+        setBoardGameOver();
+        break;
+      case '2':
+        setBoardWin();
+        break;
+      case '3':
+        transposeBoard();
+        update();
+        break;
+      case '4':
+        state = state.reverse();
+        update();
         break;
       default:
         break;
     }
   });
 });
-
-// TODO: add weight to the tiles
-// todo: save cookies?
